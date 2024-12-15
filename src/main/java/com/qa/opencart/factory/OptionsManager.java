6876af42182481a9b976d571744f38f1b5b8cf0f
Veena Hegde <veena444@gmail.com>
2024-12-15 15:08:03 +0530

    Added code to run the test cases in Selenium Grid

--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -38,13 +38,13 @@ public class OptionsManager {
 		
 		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
 			co.setCapability("browserName", "chrome");
-			co.setBrowserVersion(prop.getProperty("browserversion").trim());
-			
-			Map<String, Object> selenoidOptions = new HashMap<>();
-			selenoidOptions.put("screenResolution", "1280x1024x24");
-			selenoidOptions.put("enableVNC", true);
-			selenoidOptions.put("name", prop.getProperty("testname"));
-			co.setCapability("selenoid:options", selenoidOptions);
+//			co.setBrowserVersion(prop.getProperty("browserversion").trim());
+//			
+//			Map<String, Object> selenoidOptions = new HashMap<>();
+//			selenoidOptions.put("screenResolution", "1280x1024x24");
+//			selenoidOptions.put("enableVNC", true);
+//			selenoidOptions.put("name", prop.getProperty("testname"));
+//			co.setCapability("selenoid:options", selenoidOptions);
 		}
 		
 		return co;
@@ -63,13 +63,13 @@ public class OptionsManager {
 		}
 		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
 			fo.setCapability("browserName", "firefox");
-			fo.setBrowserVersion(prop.getProperty("browserversion").trim());
-			
-			Map<String, Object> selenoidOptions = new HashMap<>();
-			selenoidOptions.put("screenResolution", "1280x1024x24");
-			selenoidOptions.put("enableVNC", true);
-			selenoidOptions.put("name", prop.getProperty("testname"));
-			fo.setCapability("selenoid:options", selenoidOptions);
+//			fo.setBrowserVersion(prop.getProperty("browserversion").trim());
+//			
+//			Map<String, Object> selenoidOptions = new HashMap<>();
+//			selenoidOptions.put("screenResolution", "1280x1024x24");
+//			selenoidOptions.put("enableVNC", true);
+//			selenoidOptions.put("name", prop.getProperty("testname"));
+//			fo.setCapability("selenoid:options", selenoidOptions);
 		}
 		return fo;
 	}
